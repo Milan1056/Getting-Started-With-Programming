780795d036d3aa6f6ade8c25e80a3a810bd170e8
Milan1056 <milan.john0913@gmail.com>
2026-08-17 19:29:04 +0100

    birthdayhmw

---
+++ birthdayhmw.py
@@ -0,0 +1,11 @@
+mom = "16th December"
+
+dad = "24th March"
+
+brother = "1st November"
+
+print("Birthday of mom is on",mom)
+
+print("Birthday of dad is on",dad)
+
+print("Birthday of brother is on",brother)
